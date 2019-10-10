6219a2d8857198095757b700428ec844afdc855d
Mathias Mahlknecht <mathiasmahlknecht@hotmail.com>
2019-10-10 16:29:04 +0200

    add loadbalancer

--- service/Application.ts
+++ service/Application.ts
@@ -1,3 +1,5 @@
+import { RestClient } from 'typed-rest-client'
+
 import { ExpressServer } from './ExpressServer'
 import { MessageEndpoints } from './messages/MessageEndpoints'
 import { MessageService } from './messages/MessageService';
@@ -6,39 +8,43 @@ import { Environment } from './utils/Environment';
 export class Application {
     public static async createApplication() {
         const messageService = new MessageService();
-        const requestServices = {messageService}
-        const expressServer = new ExpressServer(new MessageEndpoints(),requestServices)
-        
-        await expressServer.setup(Environment.getPort())
-        Application.handleExit(expressServer)
+        const requestServices = { messageService }
+        const expressServer = new ExpressServer(new MessageEndpoints(), requestServices)
+        const port = Environment.getPort();
+        const loadBalancer = new RestClient("message-service", Environment.getLoadBalancer());
+
+        await Application.registerLoadbalancer(loadBalancer, port);
+        await expressServer.setup(port)
+
+        Application.handleExit(expressServer, loadBalancer)
 
         return expressServer
     }
 
-    private static handleExit(express: ExpressServer) {
+    private static handleExit(express: ExpressServer, loadBalancer: RestClient) {
         process.on('uncaughtException', (err: Error) => {
             console.error('Uncaught exception', err)
-            Application.shutdownProperly(1, express)
+            Application.shutdownProperly(1, express, loadBalancer)
         })
         process.on('unhandledRejection', (reason: {} | null | undefined) => {
             console.error('Unhandled Rejection at promise', reason)
-            Application.shutdownProperly(2, express)
+            Application.shutdownProperly(2, express, loadBalancer)
         })
         process.on('SIGINT', () => {
             console.info('Caught SIGINT')
-            Application.shutdownProperly(128 + 2, express)
+            Application.shutdownProperly(128 + 2, express, loadBalancer)
         })
         process.on('SIGTERM', () => {
             console.info('Caught SIGTERM')
-            Application.shutdownProperly(128 + 2, express)
+            Application.shutdownProperly(128 + 2, express, loadBalancer)
         })
         process.on('exit', () => {
             console.info('Exiting')
         })
     }
 
-    private static shutdownProperly(exitCode: number, express: ExpressServer) {
-        Promise.resolve()
+    private static shutdownProperly(exitCode: number, express: ExpressServer, loadBalancer: RestClient) {
+        Application.unregisterLoadbalancer(loadBalancer, Environment.getPort())
             .then(() => express.kill())
             .then(() => {
                 console.info('Shutdown complete')
@@ -48,5 +54,21 @@ export class Application {
                 console.error('Error during shutdown', err)
                 process.exit(1)
             })
+    }
+
+    private static async registerLoadbalancer(loadBalancer: RestClient, port: number) {
+        try {
+            await loadBalancer.get('/register/' + port);
+        } catch (err) {
+            console.error("Loadbalancer not available, coud not register. Error: " + err);
+        }
+    }
+
+    private static async unregisterLoadbalancer(loadBalancer: RestClient, port: number) {
+        try {
+            await loadBalancer.del('/unregister/' + port);
+        } catch (err) {
+            console.error("Loadbalancer not available, coud not unregister. Error: " + err);
+        }
     }
 }
--- service/utils/Environment.ts
+++ service/utils/Environment.ts
@@ -1,10 +1,15 @@
 
 export class Environment {
     public static getPort(): number {
-        return (process.env.PORT as any) || 8000
+        return (process.env.PORT as any) || 7000
     }
 
     public static getStorageAddress(): string {
-        return process.env.STORAGE_ADDRESS || "http://localhost:8081"
+        return process.env.STORAGE_ADDRESS || "http://localhost:9000"
+    }
+
+    public static getLoadBalancer(): string {
+        return process.env.LOAD_BALANCER || "http://localhost:8081"
+
     }
 }
