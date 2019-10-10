de94e4164f9c7fdd71e2b46e083dd4696d7dc073
Mathias Mahlknecht <mathiasmahlknecht@hotmail.com>
2019-10-10 12:30:22 +0200

    fix message service

--- service/messages/MessageEndpoints.ts
+++ service/messages/MessageEndpoints.ts
@@ -2,42 +2,40 @@ import { NextFunction, Request, Response } from 'express'
 import * as HttpStatus from 'http-status-codes'
 
 export class MessageEndpoints {
-    public getAllMessages = async (req: Request, res: Response, next: NextFunction ) => {
-        try{
-            res.json(req.services.messageService.getAllMessages());
+    public getAllMessages = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const messages = await req.services.messageService.getAllMessages();
+            res.json(messages);
             next();
         } catch (err) {
             next(err)
         }
     }
 
-    public getMessageById = async (req: Request, res: Response, next: NextFunction ) => {
-        try{
+    public getMessageById = async (req: Request, res: Response, next: NextFunction) => {
+        try {
             const messageId = req.params.id;
 
-            req.services.messageService.getMessage(messageId)
-                .then(message => {
-                    if(message){
-                        res.json(message);
-                    } else {
-                        res.sendStatus(HttpStatus.NOT_FOUND);
-                    }
-                    next();
-                })
+            const message = await req.services.messageService.getMessage(messageId);
+
+            if (message) {
+                res.json(message);
+            } else {
+                res.sendStatus(HttpStatus.NOT_FOUND);
+            }
+            next();
         } catch (err) {
             next(err)
         }
     }
 
-    public addMessage = async (req: Request, res: Response, next: NextFunction ) => {
-        try{
+    public addMessage = async (req: Request, res: Response, next: NextFunction) => {
+        try {
             const message = req.body.message;
 
-            req.services.messageService.addMessage(message).
-                then( m =>{
-                    res.json(m)
-                    next();
-                })
+            const response = await req.services.messageService.addMessage(message);
+            res.json(response);
+            next();
         } catch (err) {
             next(err)
         }
